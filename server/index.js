2d43f58e7938909a10161bba025a1281a966c325
Rozeta Rozin Sopkova <rrozinsopkovagmail.com@Rozetas-MacBook-Air.local>
2026-03-10 22:21:46 +0100

    Fixed all bugs

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-
+require("dotenv").config();
+ 
 app.use(express.json());
 app.use(cors());
 
@@ -20,9 +21,12 @@ const db = require("./models")
 
 
 db.sequelize.sync().then(() => {
-    app.listen(3001, () => {
+    app.listen( process.env.PORT || 3001, () => {
         console.log("Server running on port 3001");
     });
-});
+})
+    .catch((err) => {
+        console.log(err);
+    })
 
 
