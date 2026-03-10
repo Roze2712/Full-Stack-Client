b860008b2402ff09c4bf1a95f8f8c4979675862b
Rozeta Rozin Sopkova <rrozinsopkovagmail.com@Rozetas-MacBook-Air.local>
2026-03-10 20:08:03 +0100

    first commit

---
+++ server/routes/Comments.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const router = express.Router();
+const { Comments } = require("../models");
+const { validateToken } = require("../middlewares/AuthMiddleware");
+
+router.get("/:postId", async (req, res) => {
+    const postId = req.params.postId;
+    const comments = await Comments.findAll({ where: { PostId: postId }});
+    res.json(comments);
+});
+
+router.post("/", validateToken, async (req, res) => {
+  const { commentBody, PostId } = req.body;
+  const username = req.user.username;
+
+  const newComment = await Comments.create({
+    commentBody,
+    PostId,
+    username,
+  });
+
+  res.json({
+    id: newComment.id,
+    username: username,
+    commentBody: commentBody,
+  });
+});
+
+router.delete("/:commentId", validateToken, async(req, res) => {
+    const commentId = req.params.commentId;
+
+    await Comments.destroy({
+        where: {
+            id: commentId,
+        },
+    });
+
+    res.json("DELETED SUCCESSFULLY");
+});
+
+module.exports = router;
